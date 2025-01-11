9fc0f0227d615b52ff0e7cf54943c70330493fcd
jamesshow <zhenxu_usa2004@yahoo.com>
2025-01-11 15:56:18 -0500

    consume split-feature json file

--- src/main/java/com/zxu/splitio/config/SplitConfig.java
+++ src/main/java/com/zxu/splitio/config/SplitConfig.java
@@ -1,17 +1,28 @@
 package com.zxu.splitio.config;
 
-import io.split.client.SplitClient;
-import io.split.client.SplitClientConfig;
-import io.split.client.SplitFactory;
-import io.split.client.SplitFactoryBuilder;
+import io.split.client.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SplitConfig {
 
+    @Value("${split.json-file}")
+    private String splitFeatureFile;
+
     @Bean
     public SplitClient splitClient() throws Exception {
+
+        //localhost to use splitFetureFile
+        if (splitFeatureFile != null && !splitFeatureFile.isEmpty()) {
+            SplitClientConfig config = SplitClientConfig.builder()
+                    .enableDebug()
+                    .build();
+            SplitFactory splitFactory = SplitFactoryBuilder.build(splitFeatureFile, config);
+            return splitFactory.client();
+        }
+
         SplitClientConfig config = SplitClientConfig.builder()
                 .setBlockUntilReadyTimeout(10000)
                 .enableDebug()
