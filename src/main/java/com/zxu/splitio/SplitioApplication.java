8656f21fbf6099a86604daca5fe7c7e0f5c3fb36
jamesshow <zhenxu_usa2004@yahoo.com>
2025-01-10 21:45:08 -0500

    first time checkin

---
+++ src/main/java/com/zxu/splitio/SplitioApplication.java
@@ -0,0 +1,13 @@
+package com.zxu.splitio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitioApplication.class, args);
+    }
+
+}
