a2e0e2d3f7c5b11fcfcf30e52893062cabb999dc
jamesshow <zhenxu_usa2004@yahoo.com>
2025-01-11 21:35:45 -0500

    export feature to json file as notes

--- src/main/java/com/zxu/splitio/service/FeatureService.java
+++ src/main/java/com/zxu/splitio/service/FeatureService.java
@@ -1,6 +1,7 @@
 package com.zxu.splitio.service;
 
 import io.split.client.SplitClient;
+import io.split.client.api.SplitResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,15 @@ public class FeatureService {
 
     //use splitClient to load the feature and return a treatment
     public String getTreatment(String featureName, String key) {
+
+//        SplitResult result = splitClient.getTreatmentWithConfig(key, featureName);
+//        ObjectMapper mapper = new ObjectMapper();
+//        String jsonString = mapper.writeValueAsString(result);
+//        // Write to a file
+//        try (FileWriter writer = new FileWriter("feature.json")) {
+//            writer.write(jsonString);
+//        }
+
         return splitClient.getTreatment(key, featureName);
     }
 }
